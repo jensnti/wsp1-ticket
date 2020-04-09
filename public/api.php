27588ac0f2c9429f6fd5f805a57af770ee9f675d
jensnti <jens.andreasson@ga.ntig.se>
2020-04-09 10:42:10 +0200

    torsdagslektionsfråganlösdet!

--- public/api.php
+++ public/api.php
@@ -1,10 +1,35 @@
 <?php
 
-$msg = [
-    "birthdate" => "2000-01-01",
-    "price" => 1800
-];
-header('Content-type: application/json');
-echo json_encode($msg);
+include_once '../src/Ticket.php';
+
+if(isset($_POST)) {
+    // vad behöver vår server veta för att skapa en Ticket?
+
+    $birthdate = filter_input(INPUT_POST, 'birthdate', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+    $quantity = filter_input(INPUT_POST, 'quantity', FILTER_VALIDATE_INT);
+
+    // quantity birthdate
+    $ticket = new Ticket();
+    $ticket->setBirthDate($birthdate);
+    $ticket->calculateAge();
+    $ticket->calculatePrice();
+
+    $price = $ticket->getPrice();
+    $total = 0; // hur räknar jag ut total priset?
+
+    $msg = [
+        'age' => $ticket->getAge(),
+        'price' => $price,
+        'total' => $total
+    ];
+    header('Content-type: application/json');
+    echo json_encode($msg);
+}
+
+if (isset($_GET['hello'])) {
+    $msg = ['text' => 'Hello world!'];
+    header('Content-type: application/json');
+    echo json_encode($msg);
+}
 
 ?>
